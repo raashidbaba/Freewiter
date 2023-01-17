ace5d8e90cf4c8b4d6b1a4eb8e0735dcb0951e34
raashidbaba <raashidbaba849@gmail.com>
2023-01-17 23:12:33 +0530

    check my commit.

--- app/src/main/java/com/example/freewiter/MainActivity.kt
+++ app/src/main/java/com/example/freewiter/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.freewiter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("main","check my commit")
     }
 }
